d8f99f297cc6ba3a03261220fd1439771f436ae9
Junaed-Uddin <junaed.mohammed190@gmail.com>
2023-01-09 16:05:42 +0600

    firebase new app

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
@@ -9,5 +10,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_appId,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
